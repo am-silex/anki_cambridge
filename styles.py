8f18c7ad403551ca36d124a0a51bef25dc9ab07c
the-man-w-laughs <92399441+the-man-w-laughs@users.noreply.github.com>
2023-05-20 19:25:19 +0300

    Update styles.py
    Revised the HTML code by adding a hyperlink to the English word '{{Word}}', which now directs to the Cambridge Dictionary website.

--- styles.py
+++ styles.py
@@ -12,7 +12,7 @@ model_css = '''
     }'''
 
 std_word = """
-    <h3 style="text-align: center;">{{Word}}</h3>
+    <h3 style="text-align: center;"><a href="https://dictionary.cambridge.org/dictionary/english/{{Word}}">{{Word}}</a></h3>
     <blockquote>
     <div style="text-align: center;">{{Grammar}} {{Pronunciation}}</div>
     </blockquote>
@@ -22,7 +22,7 @@ std_word = """
     """
 
 std_word_def_sound = """
-    <h3 style="text-align: center;">{{Word}}</h3>    
+    <h3 style="text-align: center;"><a href="https://dictionary.cambridge.org/dictionary/english/{{Word}}">{{Word}}</a></h3>   
     <blockquote>
     <div style="text-align: center;">{{Grammar}} {{Pronunciation}}</div>
     </blockquote>
@@ -45,7 +45,7 @@ std_def_word_sound = """
     <div style="text-align: center;">{{Picture}}</div>
     <div style="text-align: center;">{{Definition}}</div>
     <hr>
-    <h3 style="text-align: center;">{{Word}}</h3>    
+    <h3 style="text-align: center;"><a href="https://dictionary.cambridge.org/dictionary/english/{{Word}}">{{Word}}</a></h3> 
     <blockquote>
     <div style="text-align: center;">{{Grammar}} {{Pronunciation}}</div>
     </blockquote>
@@ -64,7 +64,7 @@ std_sound = """
 std_sound_word_def = """
     <div style="text-align: center;">Listen.{{Audio}}</div>
     <hr>
-    <h3 style="text-align: center;">{{Word}}</h3>
+    <h3 style="text-align: center;"><a href="https://dictionary.cambridge.org/dictionary/english/{{Word}}">{{Word}}</a></h3>    
     <blockquote>
     <div style="text-align: center;">{{Grammar}} {{Pronunciation}}</div>
     </blockquote>
@@ -74,4 +74,3 @@ std_sound_word_def = """
     <div style="text-align: center;font-style: italic;">{{Examples}}</div>
     <sup style="text-align: center;">{{SynonymAntonym}}<sup>
     """
-    
